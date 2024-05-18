c3b5cf1efa3f022449f5ed7af92e8f76021a0e85
Luis Laguardia <61608109+luislaguardia4@users.noreply.github.com>
2024-05-19 02:55:41 +0800

    Create Enemy.cs

---
+++ Enemy.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float health, maxHealth = 3f;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<LootBag>().InstantiateLoot(transform.position);
+        Destroy(gameObject);
+    }
+
+}
